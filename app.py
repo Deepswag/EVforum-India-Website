bec58735ea3a9b246c2d33133aa209b4a73f18bc
Deepswag <147401589+Deepswag@users.noreply.github.com>
2023-10-10 12:27:07 +0000

    Upgraded UXUI, added some functionality

--- app.py
+++ app.py
@@ -1,11 +1,38 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [{
+    'id': 1,
+    'title': 'ABC Motors',
+    'company': ' Bajaj Motors',
+    'location': ' Bangaluru, India'
+}, {
+    'id': 2,
+    'title': 'XYZ EV Traders',
+    'company': ' Tata Motors',
+    'location': ' Delhi, India'
+}, {
+    'id': 3,
+    'title': 'Subhash Motors',
+    'company': ' Ather India',
+    'location': ' Jaipur, India'
+}, {
+    'id': 4,
+    'title': 'Avista EV Trades',
+    'company': ' Hero EV Moto',
+    'location': ' Jallandhar, India'
+}]
+
 
 @app.route("/")
-def hello_world():
-  return "Hello World"
+def EVforum_India():
+  return render_template('home.html', jobs=JOBS, company_name='EVforum India')
+
+
+@app.route("/jobs")
+def list_jobs():
+  return jsonify(JOBS)
 
 
 if __name__ == "__main__":
