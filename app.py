6eac1e138f81e8636da661a8342dae246ff6d655
Deepswag <147401589+Deepswag@users.noreply.github.com>
2023-10-11 16:12:44 +0000

    Added "Contact Us" html page, made some improvements

--- app.py
+++ app.py
@@ -36,8 +36,23 @@ def list_jobs():
 
 
 @app.route("/blog")
-def list_blog():
+def blog():
   return render_template('blog.html')
+
+
+@app.route("/about")
+def about():
+  return render_template('about.html')
+
+
+@app.route("/news")
+def news():
+  return render_template('news.html')
+
+
+@app.route("/contact")
+def contact():
+  return render_template('contact.html')
 
 
 if __name__ == "__main__":
