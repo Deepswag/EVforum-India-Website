b6a40ad17123bacd679cf98e6ab6acd3ec747ba9
Deepswag <147401589+Deepswag@users.noreply.github.com>
2023-11-21 11:19:23 +0000

    Updated Dealers Section

--- app.py
+++ app.py
@@ -2,37 +2,10 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-JOBS = [{
-    'id': 1,
-    'title': 'ABC Motors',
-    'company': ' Bajaj Motors',
-    'location': ' Bangaluru, India'
-}, {
-    'id': 2,
-    'title': 'XYZ EV Traders',
-    'company': ' Tata Motors',
-    'location': ' Delhi, India'
-}, {
-    'id': 3,
-    'title': 'Subhash Motors',
-    'company': ' Ather India',
-    'location': ' Jaipur, India'
-}, {
-    'id': 4,
-    'title': 'EV Mall Gupta Motors',
-    'company': ' Zelio',
-    'location': ' Jallandhar, India'
-}]
-
 
 @app.route("/")
 def EVforum_India():
-  return render_template('home.html', jobs=JOBS, company_name='EVforum India')
-
-
-@app.route("/jobs")
-def list_jobs():
-  return jsonify(JOBS)
+  return render_template('home.html', company_name='EVforum India')
 
 
 @app.route("/blogs")
